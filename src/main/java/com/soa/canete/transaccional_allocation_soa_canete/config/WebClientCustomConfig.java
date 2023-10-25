8aff15f3aa2b7f69f3567f60e7dd3b6db210d7bf
Edson Cárdenas Arias <72884555+edsoncardenasarias@users.noreply.github.com>
2023-10-25 11:22:14 -0500

    Ajustes en  WebClientCustomConfig.java

--- src/main/java/com/soa/canete/transaccional_allocation_soa_canete/config/WebClientCustomConfig.java
+++ src/main/java/com/soa/canete/transaccional_allocation_soa_canete/config/WebClientCustomConfig.java
@@ -1,10 +1,18 @@
 package com.soa.canete.transaccional_allocation_soa_canete.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @Configuration
 public class WebClientCustomConfig {
-    //TODO: Agregar y configura web client consumo de apis
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder()
+                .baseUrl("https:/api/transaccionalData") // Reemplaza con la URL de la API que deseas consumir
+                .defaultHeader("Authorization", "Bearer yourAccessToken"); // Agrega encabezados necesarios
+    }
 }
